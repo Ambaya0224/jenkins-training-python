b77a425ac66e1a1661582a8b3f3350ce18feb84a
Ambaya0224 <157563752+Ambaya0224@users.noreply.github.com>
2025-08-16 22:19:08 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,54 @@
-# setup.py
 from setuptools import setup, find_packages
+import os
+
+# Read long description from README.md
+current_dir = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(current_dir, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
-    name='my_simple_app', # The name of your package
-    version='0.1.0',      # The current version of your package
-    packages=find_packages(), # Automatically finds all Python packages (folders with __init__.py)
-    # You can add more metadata here if needed, like:
-    # author='Your Name',
-    # description='A simple application for DevOps assessment',
-    # long_description=open('README.md').read(),
-    # long_description_content_type='text/markdown',
-    # url='https://github.com/YourGitHubUsername/my-simple-app',
-    # classifiers=[
-    #     'Programming Language :: Python :: 3',
-    #     'License :: OSI Approved :: MIT License',
-    #     'Operating System :: OS Independent',
-    # ],
-    # python_requires='>=3.6', # Specify minimum Python version
+    name='my_simple_app',
+    version='0.1.0',
+    author='Ganbayar Ganbaatar',
+    author_email='your.email@example.com',
+    description='A simple application for DevOps assessment',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Ambaya0224/jenkins-training-python',
+    packages=find_packages(),
+    python_requires='>=3.6',
+    install_requires=[
+        # List your project dependencies here
+        # e.g., 'flask>=1.0.0',
+    ],
+    extras_require={
+        'dev': [
+            'pytest>=6.0.0',
+            'wheel',
+            'pyinstaller',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    entry_points={
+        'console_scripts': [
+            'myapp=my_simple_app.main:main',
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+    options={
+        'bdist_wheel': {
+            'universal': True
+        }
+    },
 )
